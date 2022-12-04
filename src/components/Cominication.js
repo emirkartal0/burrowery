c59d8624d6979a8f0ef30f3210bb40e2280efde3
Abdussamet <smt.3000.dgn@gmail.com>
2022-12-04 14:14:39 +0300

    form styling

--- src/components/Cominication.js
+++ src/components/Cominication.js
@@ -4,12 +4,12 @@ import { useState } from "react";
 function Cominication() {
 
     const [data, setData] = useState({
-        firstName:"",
+        firstName: "",
         lastName: "",
-        eposta:"",
-        phoneNumber:"",
-        city:"",
-        phone2Number:"",
+        eposta: "",
+        phoneNumber: "",
+        city: "",
+        phone2Number: "",
     });
 
     const sendForm = (e) => {
@@ -21,20 +21,22 @@ function Cominication() {
         <div className="pt-20 px-20 pb-10 bg-about">
             <h3 className="text-4xl font-light">İletişim / Sipariş Formu</h3>
             <p className="my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere aut reiciendis minima ipsam sunt nobis repellendus. Quisquam odit similique sapiente reiciendis molestiae, odio possimus doloremque molestias excepturi incidunt atque ipsum!, odio possimus doloremque molestias excepturi incidunt atque ipsum!</p>
-            <form onSubmit={sendForm} action="" className="flex flex-col gap-3 mt-2">
-                <div className="flex pr-6 gap-5">
-                    <input onChange={(e) => setData({ ...data, firstName: e.target.value })} placeholder="Ad" className="w-1/2 border border-acik bg-about outline-none p-2 ml-2 rounded-2xl" />
-                    <input onChange={(e) => setData({ ...data, lastName: e.target.value })} placeholder="Soyad" className="w-1/2 border border-acik bg-about outline-none p-2 ml-2 rounded-2xl" />
+            <form onSubmit={sendForm} action="" className="flex flex-col gap-3 mt-3 px-4">
+                <div className="flex items-center justify-center gap-5">
+                    <input onChange={(e) => setData({ ...data, firstName: e.target.value })} placeholder="Ad" className="w-1/2 border border-acik bg-about outline-none py-2 px-3 rounded-2xl" />
+                    <input onChange={(e) => setData({ ...data, lastName: e.target.value })} placeholder="Soyad" className="w-1/2 border border-acik bg-about outline-none py-2 px-3 rounded-2xl" />
                 </div>
-                <div className="flex items-center justify-center pr-6 gap-5">
-                    <input onChange={(e) => setData({ ...data, eposta: e.target.value })} type="email" placeholder="Eposta" className="w-1/2 border border-acik bg-about outline-none p-2 ml-2 rounded-2xl" />
-                    <input onChange={(e) => setData({ ...data, phoneNumber: e.target.value })} placeholder="Telefon" className="w-1/2 border border-acik bg-about outline-none p-2 ml-2 rounded-2xl" />
+                <div className="flex items-center justify-center gap-5">
+                    <input onChange={(e) => setData({ ...data, eposta: e.target.value })} type="email" placeholder="Eposta" className="w-1/2 border border-acik bg-about outline-none py-2 px-3 rounded-2xl" />
+                    <input onChange={(e) => setData({ ...data, phoneNumber: e.target.value })} placeholder="Telefon" className="w-1/2 border border-acik bg-about outline-none py-2 px-3 rounded-2xl" />
                 </div>
-                <div className="flex items-center justify-center pr-6 gap-5">
-                    <input onChange={(e) => setData({ ...data, city: e.target.value })} placeholder="Bulunduğunuz İl" className="w-1/2 border border-acik bg-about outline-none p-2 ml-2 rounded-2xl" />
-                    <input onChange={(e) => setData({ ...data, phone2Number: e.target.value })} placeholder="Telefon2" className="w-1/2 border border-acik bg-about outline-none p-2 ml-2 rounded-2xl" />
+                <div className="flex items-center justify-center gap-5">
+                    <input onChange={(e) => setData({ ...data, city: e.target.value })} placeholder="Bulunduğunuz İl" className="w-1/2 border border-acik bg-about outline-none py-2 px-3 rounded-2xl" />
+                    <input onChange={(e) => setData({ ...data, phone2Number: e.target.value })} placeholder="Telefon2" className="w-1/2 border border-acik bg-about outline-none py-2 px-3 rounded-2xl" />
                 </div>
-                <textarea placeholder="Mesaj" className="flex ml-2 p-5 w-full bg-about outline-none border border-acik rounded-xl" cols="30" rows="10"></textarea>
+                <div className="flex items-center justify-center">
+                    <textarea placeholder="Mesaj" className="flex  p-5 w-full bg-about outline-none border border-acik rounded-xl" cols="30" rows="10"></textarea>
+                </div>
                 <button type="submit" className="bg-koyu text-acik rounded-xl w-1/2 p-2 pl-6 font-light text-left">Formu Gönder</button>
             </form>
 
--- src/components/SSS/AccordionBar.js
+++ src/components/SSS/AccordionBar.js
@@ -14,7 +14,7 @@ function AccordionBar({ openAccordion, setOpenAccordion, accordion, index }) {
         <div className='w-10/12 px-3 pt-2 flex flex-col content-center mb-2'>
             <div className='flex w-full items-center flex-row gap-5 pb-4 border-b '>
                 <button onClick={handleSetOpen} className='p-1 hover:bg-[#efdec8] rounded-lg outline-none'>
-                    <img src={`${openAccordion == index ? "images/up-chevron.png":"images/down-chevron.png"}`} className='w-4 h-4 opacity-80'></img>
+                    <img src={`${openAccordion == index ? "images/up-chevron.png" : "images/down-chevron.png"}`} className='w-4 h-4 opacity-80'></img>
                 </button>
                 <div className='w-full'>
                     <div className='flex w-full items-center font-semibold pt-1'>{accordion?.title}</div>
@@ -24,11 +24,7 @@ function AccordionBar({ openAccordion, setOpenAccordion, accordion, index }) {
                         }
                     </div>
                 </div>
-
             </div>
-
-
-
         </div>
     )
 }
